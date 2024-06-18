8754c1ee6b40df2557bdbe29471e9dca561f8b4b
Noahw12 <90899979+Noahw12@users.noreply.github.com>
2024-06-18 16:51:07 +0200

    Improved plot function

--- kraken_test.py
+++ kraken_test.py
@@ -24,6 +24,7 @@ class KrakenReceiver():
         device.setFrequency(SOAPY_SDR_RX, 0, self.center_freq)
         device.setGain(SOAPY_SDR_RX, 0, self.gain)
         device.setBandwidth(SOAPY_SDR_RX, 0, self.bandwidth)
+        print(f"Num antennas: {device.getNumChannels(SOAPY_SDR_RX)}")
         return device
     
     def _setup_devices(self):
@@ -35,6 +36,8 @@ class KrakenReceiver():
             rx_stream = device.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32, [0])
             device.activateStream(rx_stream)
             streams[i] = rx_stream
+
+        print()
         return devices, streams
 
     def close_streams(self):
@@ -46,7 +49,7 @@ class KrakenReceiver():
         sr = self.devices[device].readStream(
             self.streams[device], [self.buffer[device]], self.num_samples, 0, time)
         
-        #print(f"Device {device}: \n Samples = {sr.ret} \n Timestamp = {sr.timeNs}\n Flag = {sr.flags}\n")
+        print(f"Device {device}: \n Samples = {sr.ret} \n Timestamp = {sr.timeNs}\n Flag = {sr.flags}\n")
 
     def read_streams(self):
         current_time_ns = int(time.time() * 1e9)
@@ -74,7 +77,7 @@ class KrakenReceiver():
         #abs_fft_ch2_samples = np.abs(fft_ch2_samples)
         
         max_index = np.argmax(abs_fft_ch0_samples)
-        # Set all elements to zero except for the one at max index
+        #Set all elements to zero except for the one at max index
 
         phase0 = np.angle(fft_ch0_samples[max_index])
         phase1 = np.angle(fft_ch1_samples[max_index])
@@ -92,67 +95,52 @@ class KrakenReceiver():
         print(doa)
         print("\n")
 
-    def plot_fft_test(self):
-        freqs = np.fft.fftfreq(self.num_samples, d=1/self.sample_rate)
+    def plot_fft(self):
 
-        ch0_samples = self.buffer[0]
-        ch1_samples = self.buffer[1]
+        if self.num_devices > 1:
+            fig, axes = plt.subplots(self.num_devices, 2, figsize=(15, 5 * self.num_devices))
+        else:
+            fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+            axes = np.array([axes])  #Ensure axes is always a 2D array
 
-        fft_ch0_samples = fft(ch0_samples)
-        fft_ch1_samples = fft(ch1_samples)
-        abs_fft_ch0_samples = np.abs(fft_ch0_samples)
-        abs_fft_ch1_samples = np.abs(fft_ch1_samples)
+        for i in range(self.num_devices):
+            #FFT of the samples
+            freqs = np.fft.fftfreq(self.num_samples, d=1/self.sample_rate)
+            #fft_samples = np.fft.fftshift(fft(self.buffer[i]))
+            fft_samples = fft(self.buffer[i])
+            axes[i, 0].plot(freqs, np.abs(fft_samples))
+            axes[i, 0].grid()
+            axes[i, 0].set_title(f'FFT of Received Samples (Channel {i})')
+            axes[i, 0].set_xlabel('Frequency (Hz)')
+            axes[i, 0].set_ylabel('Amplitude')
 
-        fft_ch0_samples[abs_fft_ch0_samples < 20] = 0
-        fft_ch1_samples[abs_fft_ch1_samples < 20] = 0
-        ch0_angle = np.angle(fft_ch0_samples, deg=True)
-        ch1_angle = np.angle(fft_ch1_samples, deg=True)
-
-        max_index = np.argmax(abs_fft_ch0_samples)
-        print(f"amplitude = {abs_fft_ch0_samples[max_index]}")
-
-        fig, ((ax, ax1), (ax_phase, ax1_phase)) = plt.subplots(2, 2, figsize=(18, 8))
-        #((ax, ax1), (ax_phase, ax1_phase)) = plt.subplots(1, 2, figsize=(15,6))
-
-        ax.plot(freqs, abs_fft_ch0_samples) 
-        ax.grid()
-        ax.set_title("Real-Time FFT of Received Samples (Channel 0)")
-        ax.set_xlabel("Frequency (Hz)")
-        ax.set_ylabel("Amplitude")
-
-        ax1.plot(freqs, abs_fft_ch1_samples)
-        ax1.grid()
-        ax1.set_title("Real-Time FFT of Received Samples (Channel 1)")
-        ax1.set_xlabel("Frequency (Hz)")
-        ax1.set_ylabel("Amplitude")
-
-
-        ax_phase.plot(freqs, ch0_angle, 'o') 
-        ax_phase.grid()
-        ax_phase.set_title("Real-Time phase of Received Samples (Channel 0)")
-        ax_phase.set_xlabel("Frequency (Hz)")
-        ax_phase.set_ylabel("phase (radians)")
-
-        ax1_phase.plot(freqs, ch1_angle, 'o')
-        ax1_phase.grid()
-        ax1_phase.set_title("Real-Time phase of Received Samples (Channel 1)")
-        ax1_phase.set_xlabel("Frequency (Hz)")
-        ax1_phase.set_ylabel("Phase (radians)")
+            #Phase of the samples
+            fft_samples[np.abs(fft_samples) < 30] = 0
+            phases = np.angle(fft_samples, deg=True)
+            phases[np.angle(fft_samples) == 0] = np.NaN
+            axes[i, 1].plot(freqs, phases, 'o')
+            axes[i, 1].grid()
+            axes[i, 1].set_xlim(-self.sample_rate / 2, self.sample_rate / 2)
+            axes[i, 1].set_ylim(-180,180)
+            axes[i, 1].set_title(f'Phase of Received Samples (Channel {i})')
+            axes[i, 1].set_xlabel('Frequency (Hz)')
+            axes[i, 1].set_ylabel('Phase (radians)')
 
         plt.tight_layout()
         plt.show()
 
 
 if __name__ == "__main__":
-    num_samples = 256*1024
-    sample_rate = 1e6
-    center_freq = 433.9e6
+    num_samples = 1000000#256*1024
+    sample_rate = 2.048e6
+    center_freq = 102.7e6
     bandwidth = 200e3
     gain = 50
     kraken = KrakenReceiver(center_freq, num_samples, 
-                            sample_rate, bandwidth, gain, num_devices=3)
+                            sample_rate, bandwidth, gain, num_devices=2)
     # while True:
     #     kraken.calc_direction_test()
     kraken.read_streams()
     kraken.close_streams()
-    kraken.plot_fft_test()
+    kraken.plot_fft()
+  
