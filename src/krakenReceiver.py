49d245728e95a8abbca36eed8381be65e15e4750
axelcerin <acke.cerin@gmail.com>
2024-07-03 12:02:08 +0200

    Circular antenna setup

--- src/krakenReceiver.py
+++ src/krakenReceiver.py
@@ -340,7 +340,7 @@ class KrakenReceiver():
         numpy.ndarray
             Array of estimated DOA angles in degrees.
         """
-        smoothed_buffer = self.spatial_smoothing(2, 'forward-backward')
+        smoothed_buffer = self.buffer #self.spatial_smoothing(2, 'forward-backward')
         spatial_corr_matrix = np.dot(smoothed_buffer, smoothed_buffer.conj().T)
         #spatial_corr_matrix = np.dot(self.buffer, self.buffer.conj().T)
         spatial_corr_matrix = pa.forward_backward_avg(spatial_corr_matrix)
@@ -650,9 +650,23 @@ if __name__ == '__main__':
     center_freq = 433e6
     bandwidth =  2e5 
     gain = 40
-    y = np.array([0,0,0])
-    x = np.array([0,1,2])
-    antenna_distance = 0.35
+    
+    # # Circular setup
+
+    ant0 = [1,    0]
+    ant1 = [0.3090,    0.9511]
+    ant2 = [-0.8090,    0.5878]
+    ant3 = [-0.8090,   -0.5878]
+    ant4 = [0.3090,   -0.9511]
+    
+    y = np.array([ant0[1], ant1[1], ant2[1], ant3[1], ant4[1]])
+    x = np.array([ant0[0], ant1[0], ant2[0], ant3[0], ant4[0]])
+    antenna_distance = 0.148857 # actual antenna distance: 0.175
+    
+    # # Linear Setup
+    # y = np.array([0, 0, 0, 0, 0])
+    # x = np.array([0, 1, 2, 3, 4])
+    # antenna_distance = 0.35
 
     kraken = KrakenReceiver(center_freq, num_samples, 
                            sample_rate, bandwidth, gain, antenna_distance, x, y, num_devices=3, simulation = 1, f_type = 'LTI', detection_range=360)
